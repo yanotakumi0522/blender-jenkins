8b6b8e0336c494d60bc75e527ffa5277953b6326
yanotakumi0522 <hw24a136@oecu.jp>
2026-07-08 15:53:26 +0900

    First commit

---
+++ export_fbx.py
@@ -0,0 +1,21 @@
+import bpy
+import sys
+
+argv = sys.argv
+argv = argv[argv.index("--") + 1:]
+
+input_path = argv[0]
+output_path = argv[1]
+
+bpy.ops.wm.open_mainfile(filepath=input_path)
+
+bpy.ops.export_scene.fbx(
+    filepath=output_path,
+    use_selection=False,
+    bake_anim=True,
+    add_leaf_bones=False,
+    axis_forward='-Z',
+    axis_up='Y'
+)
+
+print("FBX export complete")
